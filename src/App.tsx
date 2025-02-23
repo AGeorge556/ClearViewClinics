2401f1cc479b8f04f494505204f19b5cfbe0b789
AGeorge2132 <A.George2132@nu.edu.eg>
2025-02-23 18:47:07 +0200

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
             <div className="flex items-center space-x-4">
               {/* Logo */}
               <img
-                src="images\cliniclogo.jpg" 
+                src="public\images\cliniclogo.jpg" 
                 alt="Clinic Logo"
                 className="h-20 w-40"
               />
@@ -111,7 +111,7 @@ function App() {
                 ))}
               </ul>
               <img
-                src="images\Dr.Maged.jpeg"
+                src="public\images\doctor.jpg"
                 alt="Doctor"
                 className="rounded-lg shadow-lg h-[600px] object-cover"
               />
